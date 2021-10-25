e4430ef95dfe3268ce9c6a03eb03057e732695f8
AvanteCaballe <marcavante@iesebre.com>
2021-10-25 17:50:59 +0200

    Afegida funció ConnectDB

--- app/index.php
+++ app/index.php
@@ -1,23 +1,12 @@
 <?php
 
+require 'config.php';
+
 require 'app/helpers.php';
 
 require 'app/Task.php';
 
-require 'config.php';
-
-//$user = 'debian-sys-maint';
-//$pass = 'QHc3zxXIealS1vu3';
-//$dsn = 'mysql:host=localhost;dbname=phplaraveldevs';
-
-
-try {
-    $dbh = new PDO("" . ($config['database']['databasetype']) . ":host=" . ($config['database']['host']) . ";dbname=" . ($config['database']['name']) . "",
-        $config['database']['user'],
-        $config['database']['password']);
-} catch (\Exception $e) {
-    echo 'Error de connexió a la base de dades.';
-}
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * from tasks;');
 
